54ec4721c29927ddf30cefb2c154e1dd1ef42aa6
Martin Sterchi <sterchi@ifi.uzh.ch>
2019-05-03 09:34:40 +0200

    Fix bug in tests

--- tests/testthat/test_weighted_median.R
+++ tests/testthat/test_weighted_median.R
@@ -14,5 +14,5 @@ test_that("Weighted median is equal to median if weights are all 1", {
 
 test_that("Weighted median and 50% weighted quantile are equal", {
   x <- c(0.1, 0.35, 0.05, 0.1, 0.15, 0.05, 0.2)
-  expect_equal(weighted_median(x, x), weighted.quantile(x, x, 0.5), tolerance=1e-05)
+  expect_equal(weighted_median(x, x), weighted_quantile(x, x, 0.5), tolerance=1e-05)
 })
