c8ab89e08e99b8cc2dd1a8776c524378b1e0f0f0
Martin Sterchi <sterchi@ifi.uzh.ch>
2019-05-03 09:27:28 +0200

    Modify test for weighted_quantile

--- tests/testthat/test_weighted_quantile.R
+++ tests/testthat/test_weighted_quantile.R
@@ -10,5 +10,5 @@ test_that("Weighted quantile is equal to quantile if weights are all 1", {
   x <- c(0.1, 0.35, 0.05, 0.1, 0.15, 0.05, 0.2)
   w <- c(1, 1, 1, 1, 1, 1, 1)
   probs <- c(0.25, 0.5, 0.75)
-  expect_equal(weighted_quantile(x, w, probs), as.vector(quantile(x, probs)), tolerance=1e-05)
+  expect_equal(weighted_quantile(x, w, probs), as.vector(quantile(x, probs, type = 1)), tolerance=1e-05)
 })
