860e32d4ad0a60bc73600095631b5dea7ff7f540
Martin Sterchi <sterchi@ifi.uzh.ch>
2019-05-23 12:35:52 +0200

    Modify tests for weighted_median

--- tests/testthat/test_weighted_median.R
+++ tests/testthat/test_weighted_median.R
@@ -16,8 +16,3 @@ test_that("Weighted median is equal to median if weights are all 1", {
   w <- rep(1, length(x))
   expect_equal(weighted_median(x, w), median(x), tolerance=1e-05)
 })
-
-test_that("Weighted median and 50% weighted quantile are equal", {
-  x <- c(0.1, 0.35, 0.05, 0.1, 0.15, 0.05, 0.2)
-  expect_equal(weighted_median(x, x), weighted_quantile(x, x, 0.5), tolerance=1e-05)
-})
